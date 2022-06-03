80a071d799ac664dca5228c5d370230620ee2aa8
mkys9th <iat.bct.rya.cco.fmk.ys@gmail.com>
2022-06-03 13:59:29 +0900

    Monster.javaをkoutaokuboベースで作り直し

--- src/rpg/EnemyMonster.java
+++ src/rpg/EnemyMonster.java
@@ -12,7 +12,7 @@ public class EnemyMonster extends Monster{
 		// 具体的なダメージ計算式は議論の余地あり
 		Random rand = new Random();
 		double randNum = 0.15 * rand.nextDouble() + 0.85; // 0.85～1の乱数を生成
-		int damage = (int)(randNum * 10 * this.attackPower / m.getDefensePower());
+		int damage = (int)(randNum * 10 * this.getAttackPower() / m.getDefensePower());
 
 		m.setHp(m.getHp() - damage);
 		System.out.println(m.getName() + " に " + damage + " ダメージ！");
--- src/rpg/Monster.java
+++ src/rpg/Monster.java
@@ -1,67 +1,67 @@
 package rpg;
 
 public abstract class Monster {
+  protected String name;
+  protected int hp;
+  protected int max_hp;
+  protected int attack_power;
+  protected int defense_power;
+  protected boolean battle = true;
 
-	protected String name;
-	protected int hp;
-	protected int maxHp;
-	protected int attackPower;
-	protected int defensePower;
-	protected boolean battle = true;
+  public Monster(String name, int hp, int max_hp, int attack_power, int defense_power){
+    this.name = name;
+    this.hp = hp;
+    this.max_hp = max_hp;
+    this.attack_power = attack_power;
+    this.defense_power = defense_power;
+  }
 
-	public Monster(String name, int hp, int maxHp, int attackPower, int defensePower) {
-		this.name = name;
-		this.hp = hp;
-		this.maxHp = maxHp;
-		this.attackPower = attackPower;
-		this.defensePower = defensePower;
-	}
+  public void setName(String name) {
+    this.name = name;
+  }
+  public void setHp(int hp) {
+    if(hp > 0) {
+    	this.hp = hp;
+    }else {
+    	this.hp = 0;
+    }
+    this.setBattle();
+  }
+  public void setMaxHp(int max_hp) {
+    this.max_hp = max_hp;
+  }
+  public void setAttackPower(int attack_power) {
+    this.attack_power = attack_power;
+  }
+  public void setDefensPower(int defense_power) {
+    this.defense_power = defense_power;
+  }
+  public void setBattle(){
+    this.battle = (this.hp > 0);
+  }
 
-	public String toString() {
-		return ("名前： " + name + ", 残りHP： " + hp);
-	}
+  public String getName(){
+    return this.name;
+  }
+  public int getHp(){
+    return this.hp;
+  }
+  public int getMaxHp(){
+    return this.max_hp;
+  }
+  public int getAttackPower(){
+    return this.attack_power;
+  }
+  public int getDefensePower(){
+    return this.defense_power;
+  }
+  public boolean getBattle(){
+    return this.battle;
+  }
 
-	public String getName () {
-		return this.name;
-	}
-	public int getHp() {
-		return this.hp;
-	}
-	public int getMaxHp() {
-		return this.maxHp;
-	}
-	public int getAttackPower() {
-		return this.attackPower;
-	}
-	public int getDefensePower() {
-		return this.defensePower;
-	}
-	public boolean getBattle() {
-		return this.battle;
-	}
-	public void setName(String newName) {
-		this.name = newName;
-	}
-	public void setHp(int newHp) {
-		if(newHp > 0) {
-			this.hp = newHp;
-		}else {
-			this.hp = 0;
-		}
-		this.setBattle();
-	}
-	public void setMaxHp(int newMaxHp) {
-		this.maxHp = newMaxHp;
-	}
-	public void setAttackPower(int newAttackPower) {
-		this.attackPower = newAttackPower;
-	}
-	public void setDefensePower(int newDefensePower) {
-		this.defensePower = newDefensePower;
-	}
-	private void setBattle() {
-		this.battle = (this.hp > 0);
-	}
+  public abstract void attack(Monster m);
 
-	public abstract void attack(Monster m);
+  public String toString() {
+    return ("名前：" + name + " 残りHP：" + hp);
+  }
 }
