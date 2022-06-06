8308851cbeb4c11f71d46fdfe3d0d4983343b691
mkys9th <iat.bct.rya.cco.fmk.ys@gmail.com>
2022-06-06 12:59:36 +0900

    fix conflicts

--- src/rpg/MyMonster.java
+++ src/rpg/MyMonster.java
@@ -1,25 +1,5 @@
 package rpg;
 
-<<<<<<< HEAD
-import java.util.Random;
-
-public class MyMonster extends Monster{
-	public MyMonster(String name, int hp, int maxHp, int attackPower, int defensePower) {
-		super(name, hp, maxHp, attackPower, defensePower);
-	}
-
-	@Override
-	public void attack(Monster m) {
-		// 具体的なダメージ計算式は議論の余地あり
-		Random rand = new Random();
-		double randNum = 0.15 * rand.nextDouble() + 0.85; // 0.85～1の乱数を生成
-		int damage = (int)(randNum * 10 * this.getAttackPower() / m.getDefensePower());
-
-		m.setHp(m.getHp() - damage);
-		System.out.println(m.getName() + " に " + damage + " ダメージ！");
-	}
-
-=======
 public class MyMonster extends Monster{
   @Override
   public void attack(Monster enemyMonster, Monster myMonster) {
@@ -33,5 +13,4 @@ public class MyMonster extends Monster{
   public MyMonster(String name, int hp, int max_hp, int attack_power, int defense_power, int agility){
     super(name,hp,max_hp,attack_power,defense_power,agility);
   }
->>>>>>> origin/develop
 }
